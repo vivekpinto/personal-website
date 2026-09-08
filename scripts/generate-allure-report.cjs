b12495df1556f922ecd178ea1157d6f80226376c
vivekpinto <vivekpinto5@gmail.com>
2026-09-08 14:35:45 +0530

    Made changes to see the allure report

--- scripts/generate-allure-report.cjs
+++ scripts/generate-allure-report.cjs
@@ -1,17 +1,4 @@
-const fs = require('fs');
-const path = require('path');
 const { execSync } = require('child_process');
-
-const resultsDir = path.join(process.cwd(), 'allure-results');
-const reportDir = path.join(process.cwd(), 'allure-report');
-const historySource = path.join(reportDir, 'history');
-const historyDestination = path.join(resultsDir, 'history');
-
-if (fs.existsSync(historySource)) {
-  fs.cpSync(historySource, historyDestination, {
-    recursive: true,
-  });
-}
 
 execSync(
   'npx allure-commandline generate allure-results --clean -o allure-report',
